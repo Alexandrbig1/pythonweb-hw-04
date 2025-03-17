5e20e2a71d63d1fb0570f786c37a966f322faca4
Alexandrbig1 <alexsmagin1@gmail.com>
2025-03-16 19:38:18 -0500

    feat: create main py file

---
+++ main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+async def copy_file(file_path: Path, output_folder: Path):
+    pass
+
+async def read_folder(source_folder: Path, output_folder: Path):
+    pass
+
+async def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
